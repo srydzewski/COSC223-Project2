e5ae73fe3acf8ce748972202fb3ca0f4e1eb2cf5
srydzewski <srydzewski21@amherst.edu>
2019-04-04 15:45:13 -0400

    Changed job class Simulating according to littles law

--- Job.java
+++ Job.java
@@ -1,16 +1,11 @@
 public class Job{
-  private int arrivalTime;
-  private int jobSize;
-  public Job(int arrivalTime, int jobSize){
+  private double jobSize;
+  public Job(double jobSize){
     this.arrivalTime = arrivalTime;
     this.jobSize = jobSize
   }
 
   public int getJobSize(){
     return jobSize;
-  }
-
-  public int getArrivalTime(){
-    return arrivalTime;
   }
 }
--- Simulation.java
+++ Simulation.java
@@ -1,49 +1,41 @@
 import java.util.ArrayList;
 public class Simulation {
   private double lamda;
-  private int varX;
+  private double varX;
   private int numJobs;
   private int numThrowOut;
-  private int sumResponseTime;
+  private double sumArrivalsSeen;
   private ArrayList<Job> queue = new ArrayList<Job>();
-  private int arrivalTime=0;
-  private int departureTime=0;
-  private int time;
-  private Job currentJob = null;
-  //fix paramters based on Lesley's code
+  private double arrivalTime;
+  private double departureTime;
+  private double time;
+  private Job currentJob;
+
   public Simulation(double lamda, int var, int N, int throwOut){
     this.lamda = lamda;
     varX = var;
     numJobs = N;
     numThrowOut = throwOut;
-    sumResponseTime = 0;
+    sumArrivalsSeen = 0;
+    arrivalTime = generateExp(lamda);
+    departureTime = -1.0;
     time = 0;
-
-    start()
+    currentJob = null;
   }
 
-  public void start(){
-    //initialzing the queue to have a job on the server
-    int arrival0 = generateArrival(lamda);
-    int size0 = generateSize(varX);
-    Job job0 = new Job(arrival0, size0);
-    currentJob = job0;
-    time = arrival0;
-    departureTime = arrival0 + size0;
-    int arrival1 = generateArrival(lamda);
-    arrivalTime = arrival0 + arrival1;
-  }
-
-  public double run(){
+  public void run(){
     int jobsDone = 0;
     while (jobsDone < numJobs){
       //if next event is an arrival
-      if (arrivalTime < departureTime){
+      if (arrivalTime < departureTime || departureTime < 0){
         time = arrivalTime;
+        //add how many jobs are before the arrival
+        if (jobsDone >= numThrowOut){
+          sumArrivalsSeen += queue.size();
+        }
         //generate new job
-        int new_arrival = generateArrival(lamda);
-        int new_size = generateSize(varX);
-        Job new_job = new Job(time+new_arrival,new_size);
+        double new_size = generateHyperExp(varX);
+        Job new_job = new Job(new_size);
         //if server is idle
         if (currentJob == null){
           currentJob = new_job;
@@ -53,15 +45,33 @@ public class Simulation {
         else{
           queue.add(new_job);
         }
-        arrivalTime = time + generateArrival(lamda);
+        arrivalTime = time + generateExp(lamda);
       }
+      //if next event is a departure
       else {
         time = departureTime;
-
+        //if nothing in the queue
+        if (queue.size() == 0){
+          currentJob = null;
+          //setting departureTime < 0 since a departure can't happen
+          departureTime = -1.0;
+        }
+        //otherwise put first job in queue on server
+        else {
+          Job upNext = queue.remove(0);
+          currentJob = upNext;
+          depatureTime = time + currentJob.getJobSize();
+        }
+        jobsDone++;
       }
-
     }
   }
 
+  public double getAvgResponseTime(){
+    run();
+    //finding E[T] by Little's Law
+    double avgJobsBefore = sumArrivalsSeen/(numJobs - numThrowOut);
+    return avgJobsBefore/lamda;
+  }
 
 }
